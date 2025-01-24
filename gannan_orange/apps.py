fc20d962f20392990261941da32f61ab868e07e4
EstyZhong <EstyZhong@outlook.com>
2025-01-24 15:51:16 +0800

    feat(gannan_orange): 添加应用的友好名称
    - 在 GannanOrangeConfig 类中添加 verbose_name 属性
    - 设置应用的友好名称为"脐橙库"

--- gannan_orange/apps.py
+++ gannan_orange/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class GannanOrangeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'gannan_orange'
+    verbose_name = '脐橙库'
