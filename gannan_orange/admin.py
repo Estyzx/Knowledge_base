1be248c1644dd068460ca90fcc7cd70d911d3d48
EstyZhong <EstyZhong@outlook.com>
2025-01-24 15:34:23 +0800

    feat(admin): 注册模型到 admin 后台
    - 引入 Variety、PlantingTech 和 Pest模型
    - 在 admin 后台注册这三个模型，使它们可以被管理和操作

--- gannan_orange/admin.py
+++ gannan_orange/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from models import Variety,PlantingTech,Pest
 # Register your models here.
+
+
+
+
+# 注册模型
+admin.site.register(Variety)
+admin.site.register(PlantingTech)
+admin.site.register(Pest)
