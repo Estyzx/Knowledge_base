50dce65107ee7a8c100d0309e85f4bb2453b4f88
EstyZhong <EstyZhong@outlook.com>
2025-01-24 16:14:22 +0800

    feat(gannan_orange): 添加首页功能
    - 新增 HomePage 类继承 TemplateView，用于渲染首页- 在 urls.py 中配置首页路由
    - 更新 views.py，添加 Variety 和 PlantingTech 模型的导入
    - 实现首页上下文数据的获取，包括品种总数、最新品种和最新种植技术
    - 注释掉的代码为未来可能添加的用户推荐功能

--- gannan_orange/views.py
+++ gannan_orange/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
+from .models import Variety, PlantingTech
 # Create your views here.
+
+
+class HomePage(TemplateView):
+    template_name = 'gannan_orange/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context.update({
+            'total_variety' : Variety.objects.count(),
+            'last_variety': Variety.objects.order_by('-create_time')[:3],
+            'last_planting_tech': PlantingTech.objects.order_by('-create_time')[:3],
+        })
+
+        # if self.request.user.is_authenticated:
+        #     context['user'] = self.request.user
+        #     user_data = self.get_user_recommend()
+
+        return context
+
+
+"""
+    def get_user_recommend(self):
+        user = self.request.user
+        context = {}
+        if user.region:
+            context['region'] = user.region
+        if user.variety:
+            context['variety'] = user.variety
+
+        return context
+"""
+
+
