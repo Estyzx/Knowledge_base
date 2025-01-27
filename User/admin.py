277fe717622422ba2457037f96ef4a9f30d241c8
EstyZhong <EstyZhong@outlook.com>
2025-01-27 23:42:50 +0800

    feat(user): 创建自定义用户模型并集成到 admin
    - 新增 CustomUser 模型，包含用户名、手机号和地址字段
    - 在 admin 中注册 CustomUser，并定制显示和编辑字段
    - 更新 User应用的配置，设置 verbose_name 为"用户管理"
    - 在 settings 中指定自定义用户模型为 AUTH_USER_MODEL

--- User/admin.py
+++ User/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import  CustomUser
 # Register your models here.
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'phone', 'location')
+    exclude = ('password','date_joined', 'last_login')
+    readonly_fields = ('date_joined', 'last_login')
+    fieldsets = (
+        ('个人信息',{'fields': ('username', 'phone', 'location')}),
+        ('权限',{'fields': ('is_staff', 'is_active', 'is_superuser')}),
+        ('其他',{'fields': ('date_joined', 'last_login')})
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- User/models.py
+++ User/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    username = models.CharField('用户名',max_length=50,unique=True)
+    phone = models.CharField('手机号',max_length=11,unique=True)
+    location = models.CharField('地址',max_length=50,blank=True)
+
+
+    class Meta:
+
+        verbose_name = '用户'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.username
