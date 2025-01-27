025e4543a6c0f64c0a826c305eaef4706cbf5ae5
EstyZhong <EstyZhong@outlook.com>
2025-01-27 19:19:23 +0800

    admin(gannan_orange):优化品种列表和详情页面展示- 在 admin 中添加 VarietyAdmin 类，增加列表展示字段和自定义方法 - 修改品种详情模板，调整标题居中并优化种植技术展示逻辑

--- gannan_orange/admin.py
+++ gannan_orange/admin.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
-from .models import Variety,PlantingTech,Pest
+from .models import Variety, PlantingTech, Pest
+
+
 # Register your models here.
 
+class VarietyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'tech_names', 'pest_names')
 
+    def tech_names(self, obj):
+        return ", ".join([tech.name for tech in obj.planting_tech.all()])
+
+    def pest_names(self, obj):
+        return ", ".join([pest.name for pest in obj.pest.all()])
 
 
 # 注册模型
-admin.site.register(Variety)
+admin.site.register(Variety, VarietyAdmin)
 admin.site.register(PlantingTech)
 admin.site.register(Pest)
